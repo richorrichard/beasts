6fd4f466b09d841215b1831ed2070e9d0c2e1aa0
Richard Sherman <hello@richardsherman.com>
2019-11-12 11:22:01 -0700

    toFixed(): added test to check for adding fill `0`s, passed it

--- 07/07-toFixed.js
+++ 07/07-toFixed.js
@@ -10,26 +10,25 @@ function checkPrecision(val, base) {
 }
 
 /**
- * Rounding a decimal to certain precision. This implementation is limited by 
- * available digits, and will not compute additional decimal places if required. 
+ * Rounds a number to certain `precision` and returns number as a string. It will pad the returned decimal string 
+ * to fix it on the exact number of decimal points specified by `precision`.
  * 
  * @param {number} value 
  * @param {number} precision
  * 
- * @return {number}
+ * @return {string}
  */
  
 function toFixed(value, precision) {
   precision = checkPrecision(precision, precisionBase);
   
   var strValue = String(value); 
-  var decimalIndex;
-
-  decimalIndex = strValue.indexOf('.');
+  var decimalIndex = strValue.indexOf('.');
+  var afterLength = strValue.substr(decimalIndex+1).length
   
   // Limitation of this method of decimal placement. Cannot calculate places longer than string
-  if (precision > strValue.substr(decimalIndex+1).length) {
-    return value;
+  if (precision > afterLength) {
+    strValue = strValue + '0'.repeat(precision - afterLength);
   }
 
   // Move decimal `precision` places
@@ -41,45 +40,48 @@ function toFixed(value, precision) {
   strValue = String(value);
 
   // Return decimal to original location
-  strValue = strValue.substr(0, strValue.length - precision) + '.' + strValue.substr(strValue.length - precision);
-
-  return parseFloat(strValue);  
+  if (precision > 0) {
+    strValue = strValue.substr(0, strValue.length - precision) + '.' + strValue.substr(strValue.length - precision);
+  } else {
+    strValue = strValue.substr(0, strValue.length - precision);
+  }
+  return strValue;  
 }
 
 tests({
 
-  'It should return `value` rounded to `precision` decimal places.': function() {
+  'It should return `strValue` rounded to `precision` decimal places.': function() {
     var resultNum = toFixed(1.149, 2);
-    eq(resultNum, 1.15);
+    eq(resultNum, '1.15');
   },
   'If `precision` is NaN, it should reference the default base precision.': function() {
-    var resultNum = toFixed(1.5, (0/1));
-    eq(resultNum, 2);
+    var resultNum = toFixed(1.5, NaN);
+    eq(resultNum, '2');
   },
   'If `precision` is negative, it should convert to a positive precision.': function() {
     var resultNum = toFixed(1.555, -2);
-    eq(resultNum, 1.56);
+    eq(resultNum, '1.56');
   },
   'It should work with very large decimals': function() {
     var resultNum = toFixed(12345.12345, 2);
-    eq(resultNum, 12345.12);
+    eq(resultNum, '12345.12');
   },
   'It should round `0.615` to `0.62`.': function() {
     var resultNum = toFixed(.615, 2);
-    eq(resultNum, .62);
+    eq(resultNum, '.62');
   },
   'It should round `10.235` to `10.24`.': function() {
     var resultNum = toFixed(10.235, 2);
-    eq(resultNum, 10.24);
+    eq(resultNum, '10.24');
   },
   'It should round `1.005` to `1.01`.': function() {
     var resultNum = toFixed(1.005, 2);
-    eq(resultNum, 1.01);
+    eq(resultNum, '1.01');
   },
-  'If `precision > afterStr.length`, return value': function() {
+  'If `precision > afterStr.length`, pad result decimal places to match precision required.': function() {
     debugger;
     var resultNum = toFixed(1.125, 6);
-    eq(resultNum, 1.125);
+    eq(resultNum, '1.125000');
   }
 
 });
